ea443b3212d708b28e86886447dfac5635e9922c
Jonas Paulus <jopaulus@students.uni-mainz.de>
2025-03-08 13:05:20 +0100

    adding print_polygon

--- sigil.py
+++ sigil.py
@@ -7,9 +7,16 @@ def print_circle(ax, r):
     y = np.cos(phi)*r
     ax.plot(x,y)
 
+def print_polygon(ax, n,r, phi0 = -np.pi/2):
+    phi = np.linspace(0,2*np.pi,n+1) + phi0
+    x = np.cos(phi)*r
+    y = np.sin(phi)*r
+    ax.plot(x,y)
+
 
 print("hello world")
 fig, ax = plt.subplots()
 print_circle(ax, 1)
+print_polygon(ax, 5, 1)
 plt.show()
 
